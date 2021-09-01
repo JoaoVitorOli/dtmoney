4b5a590a987fa156200ec4781fd38f43d18af9e6
João Vitor <infamoz_br3@hotmail.com>
2021-08-31 23:35:30 -0300

    chore: header feito

---
+++ src/components/header/index.tsx
@@ -0,0 +1,19 @@
+import { Container, Content } from "./styles";
+
+import logoImg from "../../assets/logo.svg";
+
+export function Header() {
+  return(
+    <Container>
+      <Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button">
+          Nova transação
+        </button><img src={logoImg} alt="dt money" />
+        <button type="button">
+          Nova transação
+        </button>
+      </Content> 
+    </Container>
+  );
+}
